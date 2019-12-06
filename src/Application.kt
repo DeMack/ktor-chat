56f3881fb23d7606056aadea4f05b4577a41c984
DanielMc <daniel_mckerrocher@ultimatesoftware.com>
2019-12-06 12:14:59 -0500

    Initial Commit

---
+++ src/Application.kt
@@ -0,0 +1,24 @@
+package com.github.demack
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+  install(CallLogging)
+
+  routing {
+    get("/") {
+      call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+    }
+  }
+}
